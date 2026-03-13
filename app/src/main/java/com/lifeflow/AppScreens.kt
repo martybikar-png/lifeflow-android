eb1d3811b95d89a5560b8cccadbb2ad15264a758
Marty <martybikar@gmail.com>
2026-03-13 20:30:34 +0000

    Add LifeFlow icon to loading screen

--- app/src/main/java/com/lifeflow/AppScreens.kt
+++ app/src/main/java/com/lifeflow/AppScreens.kt
@@ -1,17 +1,21 @@
 package com.lifeflow
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.lifeflow.core.HealthConnectUiState
 import com.lifeflow.domain.core.digitaltwin.DigitalTwinState
@@ -31,6 +35,19 @@ fun LoadingScreen(
     debugLines: List<String>
 ) {
     ScreenContainer(title = "LifeFlow") {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.lifeflow_one_icon),
+                contentDescription = "LifeFlow One icon",
+                modifier = Modifier.size(96.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
         Text(
             text = loadingMessage(
                 isAuthenticating = isAuthenticating,
