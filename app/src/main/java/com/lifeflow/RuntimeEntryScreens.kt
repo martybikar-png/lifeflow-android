20f704dff74f3834fcaca82259c10e122460b157
Marty <249821796+martybikar-png@users.noreply.github.com>
2026-04-28 19:53:37 +0100

    Remove legacy loading transition screen

--- app/src/main/java/com/lifeflow/RuntimeEntryScreens.kt
+++ app/src/main/java/com/lifeflow/RuntimeEntryScreens.kt
@@ -1,8 +1,6 @@
 package com.lifeflow
 
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
