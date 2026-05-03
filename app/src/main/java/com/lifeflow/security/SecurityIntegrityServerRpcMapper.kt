b62ca85fe30e5315fbf5ca637ecb8d2bf4e6e2d2
Marty <249821796+martybikar-png@users.noreply.github.com>
2026-05-03 10:44:42 +0100

    Reuse integrity trust RPC claims mapper

--- app/src/main/java/com/lifeflow/security/SecurityIntegrityServerRpcMapper.kt
+++ app/src/main/java/com/lifeflow/security/SecurityIntegrityServerRpcMapper.kt
@@ -15,7 +15,7 @@ internal class SecurityIntegrityServerRpcMapper {
             serverTimestampEpochMs = response.serverTimestampEpochMs,
             policyVersion = response.policyVersion,
             verdictSource = mapSource(response.verdictSource),
-            claims = mapClaims(response.claims),
+            claims = mapSecurityIntegrityVerdictClaimsFromRpc(response.claims),
             attestationVerification = mapAttestationVerification(
                 response.attestationVerification
             ),
@@ -58,75 +58,6 @@ internal class SecurityIntegrityServerRpcMapper {
             IntegrityTrustRpcDecision.DENY -> IntegrityTrustDecision.DENY
             IntegrityTrustRpcDecision.LOCK -> IntegrityTrustDecision.LOCK
         }
-    }
-
-    private fun mapClaims(
-        claims: IntegrityTrustRpcClaims
-    ): SecurityIntegrityVerdictClaims {
-        return SecurityIntegrityVerdictClaims(
-            appRecognitionVerdict = claims.appRecognitionVerdict?.let { rpcValue ->
-                when (rpcValue) {
-                    IntegrityTrustRpcAppRecognitionVerdict.PLAY_RECOGNIZED ->
-                        SecurityIntegrityAppRecognitionVerdict.PLAY_RECOGNIZED
-
-                    IntegrityTrustRpcAppRecognitionVerdict.UNRECOGNIZED_VERSION ->
-                        SecurityIntegrityAppRecognitionVerdict.UNRECOGNIZED_VERSION
-
-                    IntegrityTrustRpcAppRecognitionVerdict.UNEVALUATED ->
-                        SecurityIntegrityAppRecognitionVerdict.UNEVALUATED
-                }
-            },
-            deviceRecognitionVerdicts = claims.deviceRecognitionVerdicts.mapTo(
-                linkedSetOf()
-            ) { rpcValue ->
-                when (rpcValue) {
-                    IntegrityTrustRpcDeviceRecognitionVerdict.MEETS_BASIC_INTEGRITY ->
-                        SecurityIntegrityDeviceRecognitionVerdict.MEETS_BASIC_INTEGRITY
-
-                    IntegrityTrustRpcDeviceRecognitionVerdict.MEETS_DEVICE_INTEGRITY ->
-                        SecurityIntegrityDeviceRecognitionVerdict.MEETS_DEVICE_INTEGRITY
-
-                    IntegrityTrustRpcDeviceRecognitionVerdict.MEETS_STRONG_INTEGRITY ->
-                        SecurityIntegrityDeviceRecognitionVerdict.MEETS_STRONG_INTEGRITY
-
-                    IntegrityTrustRpcDeviceRecognitionVerdict.MEETS_VIRTUAL_INTEGRITY ->
-                        SecurityIntegrityDeviceRecognitionVerdict.MEETS_VIRTUAL_INTEGRITY
-                }
-            },
-            appLicensingVerdict = claims.appLicensingVerdict?.let { rpcValue ->
-                when (rpcValue) {
-                    IntegrityTrustRpcAppLicensingVerdict.LICENSED ->
-                        SecurityIntegrityAppLicensingVerdict.LICENSED
-
-                    IntegrityTrustRpcAppLicensingVerdict.UNLICENSED ->
-                        SecurityIntegrityAppLicensingVerdict.UNLICENSED
-
-                    IntegrityTrustRpcAppLicensingVerdict.UNEVALUATED ->
-                        SecurityIntegrityAppLicensingVerdict.UNEVALUATED
-                }
-            },
-            playProtectVerdict = claims.playProtectVerdict?.let { rpcValue ->
-                when (rpcValue) {
-                    IntegrityTrustRpcPlayProtectVerdict.NO_ISSUES ->
-                        SecurityIntegrityPlayProtectVerdict.NO_ISSUES
-
-                    IntegrityTrustRpcPlayProtectVerdict.NO_DATA ->
-                        SecurityIntegrityPlayProtectVerdict.NO_DATA
-
-                    IntegrityTrustRpcPlayProtectVerdict.POSSIBLE_RISK ->
-                        SecurityIntegrityPlayProtectVerdict.POSSIBLE_RISK
-
-                    IntegrityTrustRpcPlayProtectVerdict.MEDIUM_RISK ->
-                        SecurityIntegrityPlayProtectVerdict.MEDIUM_RISK
-
-                    IntegrityTrustRpcPlayProtectVerdict.HIGH_RISK ->
-                        SecurityIntegrityPlayProtectVerdict.HIGH_RISK
-
-                    IntegrityTrustRpcPlayProtectVerdict.UNEVALUATED ->
-                        SecurityIntegrityPlayProtectVerdict.UNEVALUATED
-                }
-            }
-        )
     }
 
     private fun mapAttestationVerification(
