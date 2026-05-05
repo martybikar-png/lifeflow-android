16a2c650fdf01772dda47e5918a3146235cf00fa
Marty <249821796+martybikar-png@users.noreply.github.com>
2026-05-05 17:11:56 +0100

    Clean system splash startup

--- app/src/main/java/com/lifeflow/MainActivity.kt
+++ app/src/main/java/com/lifeflow/MainActivity.kt
@@ -1,13 +1,7 @@
 package com.lifeflow
 
 import android.content.Intent
-import android.graphics.Color
-import android.widget.ImageView
-import android.view.Gravity
 import android.os.Bundle
-import android.view.View
-import android.view.ViewTreeObserver
-import android.widget.FrameLayout
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
@@ -35,32 +29,7 @@ class MainActivity : FragmentActivity() {
 
         val onboardingStore = OnboardingStateStore(this)
 
-        showNativeStartupFrame()
-        window.decorView.runAfterFirstWindowFocus {
-            installComposeContent(onboardingStore = onboardingStore)
-        }
-    }
-
-    private fun showNativeStartupFrame() {
-        val iconSizePx = (132 * resources.displayMetrics.density).toInt()
-
-        setContentView(
-            FrameLayout(this).apply {
-                setBackgroundColor(Color.rgb(34, 205, 247))
-                addView(
-                    ImageView(this@MainActivity).apply {
-                        setImageResource(R.drawable.lifeflow_splash_icon)
-                        adjustViewBounds = true
-                        scaleType = ImageView.ScaleType.FIT_CENTER
-                    },
-                    FrameLayout.LayoutParams(
-                        iconSizePx,
-                        iconSizePx,
-                        Gravity.CENTER
-                    )
-                )
-            }
-        )
+        installComposeContent(onboardingStore = onboardingStore)
     }
 
     private fun installComposeContent(onboardingStore: OnboardingStateStore) {
@@ -77,32 +46,6 @@ class MainActivity : FragmentActivity() {
                 }
             }
         }
-    }
-}
-
-private fun View.runAfterFirstWindowFocus(action: () -> Unit) {
-    var didPost = false
-    val target = this
-    val observer = viewTreeObserver
-
-    val listener = object : ViewTreeObserver.OnWindowFocusChangeListener {
-        override fun onWindowFocusChanged(hasFocus: Boolean) {
-            if (!hasFocus || didPost) return
-
-            didPost = true
-            target.post {
-                if (observer.isAlive) {
-                    observer.removeOnWindowFocusChangeListener(this)
-                }
-                action()
-            }
-        }
-    }
-
-    observer.addOnWindowFocusChangeListener(listener)
-
-    if (hasWindowFocus()) {
-        listener.onWindowFocusChanged(true)
     }
 }
 
